42581ebc55d3ab97446d0ad11da7a0f2aa82dc68
Arsy Nizlan Ramadhan <arsynizlanr@gmail.com>
2022-10-19 07:09:15 +0700

    Arsy - Add Authentication with Livewere

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.auth')
+@section('login')
+    <div class="card">
+        <div class="card-body">
+
+            <a href="{{ url('/') }}">
+                <h4 class="mb-2">POS APP</h4>
+            </a>
+
+            <p class="mb-4">Please sign-in to your account</p>
+
+            <x-jet-validation-errors class="alert alert-danger alert-dismissible" />
+
+
+            <form id="formAuthentication" class="mb-3" action="{{ route('login') }}" method="POST">
+                @csrf
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="text" class="form-control" wire:model="email" id="email" name="email"
+                        placeholder="Enter your email" autofocus value="{{ old('email') }}" />
+                    @error('email')
+                        <span class="error">{{ $message }}</span>
+                    @enderror
+                </div>
+                <div class="mb-3 form-password-toggle">
+                    <div class="d-flex justify-content-between">
+                        <label class="form-label" for="password">Password</label>
+                        <a href="auth-forgot-password-basic.html">
+                            <small>Forgot Password?</small>
+                        </a>
+                    </div>
+                    <div class="input-group input-group-merge">
+                        <input type="password" id="password" class="form-control" wire:model="password" name="password"
+                            placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                            aria-describedby="password" />
+                        <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                    </div>
+                    @error('password')
+                        <span class="error">{{ $message }}</span>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" id="remember-me" />
+                        <label class="form-check-label" for="remember-me"> Remember Me </label>
+                    </div>
+                </div>
+                <div class="mb-3">
+                    <button class="btn btn-primary d-grid w-100" type="submit">Sign in</button>
+                </div>
+            </form>
+
+            <p class="text-center">
+                <span>New on our platform?</span>
+                <a href="auth-register-basic.html">
+                    <span>Create an account</span>
+                </a>
+            </p>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,131 @@
+<div class="layout-container">
+    <!-- Menu -->
+
+    <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+        <div class="app-brand demo">
+            <a href="index.html" class="app-brand-link">
+                <span class="app-brand-text menu-text fw-bolder">POS APP</span>
+            </a>
+
+            <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
+                <i class="bx bx-chevron-left bx-sm align-middle"></i>
+            </a>
+        </div>
+
+        <div class="menu-inner-shadow"></div>
+
+        <ul class="menu-inner py-1">
+            <!-- Dashboard -->
+            <li class="menu-item active">
+                <a href="index.html" class="menu-link">
+                    <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                    <div data-i18n="Analytics">Dashboard</div>
+                </a>
+            </li>
+
+
+            <li class="menu-header small text-uppercase">
+                <span class="menu-header-text">MASTER</span>
+            </li>
+
+            <li class="menu-item">
+                <a href="index.html" class="menu-link">
+                    <i class='menu-icon tf-icons bx bx-category-alt'></i>
+                    <div data-i18n="Analytics">Kategori</div>
+                </a>
+            </li>
+
+            <li class="menu-item">
+                <a href="index.html" class="menu-link">
+                    <i class='menu-icon tf-icons bx bx-package'></i>
+                    <div data-i18n="Analytics">Produk</div>
+                </a>
+            </li>
+
+            <li class="menu-item">
+                <a href="index.html" class="menu-link">
+                    <i class='menu-icon tf-icons bx bx-user'></i>
+                    <div data-i18n="Analytics">Member</div>
+                </a>
+            </li>
+
+            <li class="menu-item">
+                <a href="index.html" class="menu-link">
+                    <i class='menu-icon tf-icons bx bxs-truck'></i>
+                    <div data-i18n="Analytics">Supplier</div>
+                </a>
+            </li>
+
+            <li class="menu-header small text-uppercase">
+                <span class="menu-header-text">TRANSAKSI</span>
+            </li>
+
+            <li class="menu-item">
+                <a href="index.html" class="menu-link">
+                    <i class='menu-icon tf-icons bx bx-money'></i>
+                    <div data-i18n="Analytics">Pengeluaran</div>
+                </a>
+            </li>
+
+            <li class="menu-item">
+                <a href="index.html" class="menu-link">
+                    <i class='menu-icon tf-icons bx bxs-down-arrow-square'></i>
+                    <div data-i18n="Analytics">Pembelian</div>
+                </a>
+            </li>
+
+            <li class="menu-item">
+                <a href="index.html" class="menu-link">
+                    <i class='menu-icon tf-icons bx bxs-up-arrow-square'></i>
+                    <div data-i18n="Analytics">Penjualan</div>
+                </a>
+            </li>
+
+            <li class="menu-item">
+                <a href="index.html" class="menu-link">
+                    <i class='menu-icon tf-icons bx bx-transfer-alt'></i>
+                    <div data-i18n="Analytics">Transaksi Lama</div>
+                </a>
+            </li>
+
+            <li class="menu-item">
+                <a href="index.html" class="menu-link">
+                    <i class='menu-icon tf-icons bx bx-transfer-alt'></i>
+                    <div data-i18n="Analytics">Transaksi Baru</div>
+                </a>
+            </li>
+
+            <li class="menu-header small text-uppercase">
+                <span class="menu-header-text">REPORT</span>
+            </li>
+
+            <li class="menu-item">
+                <a href="index.html" class="menu-link">
+                    <i class='menu-icon tf-icons bx bxs-file'></i>
+                    <div data-i18n="Analytics">Laporan</div>
+                </a>
+            </li>
+
+            <li class="menu-header small text-uppercase">
+                <span class="menu-header-text">SYSTEM</span>
+            </li>
+
+            <li class="menu-item">
+                <a href="index.html" class="menu-link">
+                    <i class='menu-icon tf-icons bx bxs-cog'></i>
+                    <div data-i18n="Analytics">User</div>
+                </a>
+            </li>
+
+            <li class="menu-item">
+                <a href="index.html" class="menu-link">
+                    <i class='menu-icon tf-icons bx bxs-cog'></i>
+                    <div data-i18n="Analytics">Pengaturan</div>
+                </a>
+            </li>
+
+
+
+        </ul>
+    </aside>
+    <!-- / Menu -->
